257d0d17eeb04b17618936e9c13a080ca12be96c
gmlesher <70789983+gmlesher@users.noreply.github.com>
2021-11-16 16:51:41 -0500

    delete after parse and write csv files to project root
    Updated parse.py to get latest file in downloads folder to parse, then delete after finished parsing. force project root path writing of csv files.

--- bot/parse.py
+++ bot/parse.py
@@ -6,18 +6,20 @@ class Parse():
     """Parse class"""
     def __init__(self):
         self.data = None
-        self.read_path = '/Users/garrettlesher/Downloads'
-        self.extension = 'csv'
+        self.read_path = '/Users/garrettlesher/Downloads/'
+        self.extension = '*.csv'
 
     def parse_csv(self):
         """parses most recent csv files in downloads folder"""
         print('Parsing downloaded csv file...')
-        os.chdir(self.read_path)
-        result = glob.glob(f'*.{self.extension}')
-
-        for file in result:
-            df = pd.read_csv(file)
-            self.data = df.T.to_dict('dict')
+        files = glob.glob(self.read_path + self.extension)
+        latest_file = max(files, key=os.path.getctime)
+        df  = pd.read_csv(latest_file)
+        self.data = df.T.to_dict('dict')
+        try:
+            os.remove(latest_file) # removes the file
+        except OSError:
+            pass
 
     def run(self):
         """runs Parse class"""
