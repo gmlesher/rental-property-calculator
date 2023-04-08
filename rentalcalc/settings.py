b0cc0754d7c8fb639c1e6304efe9a6a092889a72
gmlesher <70789983+gmlesher@users.noreply.github.com>
2023-04-08 08:05:08 -0500

    use postgres db

--- rentalcalc/settings.py
+++ rentalcalc/settings.py
@@ -93,9 +93,18 @@ WSGI_APPLICATION = 'rentalcalc.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.environ.get("DATABASE_NAME"),
+        "USER": os.environ.get("DATABASE_USER"),
+        "PASSWORD": os.environ.get("DATABASE_PASSWORD"),
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
